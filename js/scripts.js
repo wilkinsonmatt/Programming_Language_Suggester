39dc5a1a68413825ccd7697e878ee80931339c0d
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-03 09:10:44 -0700

    added interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,15 @@ function logicBoard(A, B, C, D, F) {
 
 
 
+$(document).ready(function() {
+  $("form#-----").submit(function(event) {
+    event.preventDefault();
+    const answerOne = parseInt($("#answer1").val());
+    const answerTwo = parseInt($("#answer2").val());
+    const answerThree = parseInt($("#answer3").val());
+    const answerFour = parseInt($("#answer4").val());
+    const answerFive = parseInt($("#answer5").val());
+    const result = logicBoard(answerOne, answerTwo, answerThree, answerFour, answerFive);
+    $("#output").text(result);
+  });
+});
